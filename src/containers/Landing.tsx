3d62f12e33f2ddadae548ee55ab80c1bdb848279
mslifelineway <mukeshms40003@gmail.com>
2022-09-16 23:32:56 +0530

    color-code is defined in css variable

---
+++ src/containers/Landing.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <section className="header">
+        <div className="header__logo">
+          <img src="" alt="header logo" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Landing;
